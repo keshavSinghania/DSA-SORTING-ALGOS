12f39f9cc937da2451adb1ce03b5956cc9fd1368
Keshav Singhania <worksinghania@gmail.com>
2025-08-30 14:23:11 +0530

    Selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class SelectionSort {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of you array");
+        int n = sc.nextInt();
+
+        int[] myArr = new int[n];
+        System.out.println("Enter the elements of your array");
+
+        for(int i=0; i<n; i++){
+            myArr[i] = sc.nextInt();
+        }
+
+        sort(myArr, n);
+
+        System.out.println(Arrays.toString(myArr));
+    }
+
+    //function to sort array using selection sort
+    public static void sort(int[] arr, int n){
+        for(int i=0; i<n-1; i++){
+            int minInd = i;
+            for(int j=i; j<n; j++){
+                if(arr[minInd] > arr[j]){
+                    minInd = j;
+                }
+            }
+            //swap with minimum index
+            int temp = arr[minInd];
+            arr[minInd] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
